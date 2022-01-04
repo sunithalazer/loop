9e1e8ac606ffe3f1cda29b64be8ba64c7e97e634
sunithalazer <97090049+sunithalazer@users.noreply.github.com>
2022-01-04 14:44:07 +0530

    Add files via upload

---
+++ sunitha's loop.py
@@ -0,0 +1,871 @@
+       #     even & odd numbers
+# i=1
+# while i<=10:
+#     if i%2==0:
+#       print(i,"even number....")
+#     else:
+#         print(i,"odd number.....")
+#     i+=1
+
+# natural numbers....
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+ 
+    # whole numbers ......
+# i=0  
+# while i<=10:
+#     print(i)
+#     i+=1
+
+    #  integers and squares......
+# i=1
+# while i<=10:
+#     print(i,i*i)
+#     i+=1
+
+        #  following series.....10,20,30,40,.........300
+# i=10
+# while i<=300:
+#     # i used end=""    for row wise answer ......
+#     print(i,end=" ")      
+#     i+=10
+
+          # positive odd and negative even....
+# a=1
+# while a<=100:
+#     if a%2==0:
+#         print(-a,end=" ")
+#     else:
+#         print(a , end=" ")
+#     a+=1
+
+# count=1
+# while count<=5:
+#  print("working")
+#  count=count+1
+
+# a=891
+# while a<=931:
+#     d=a-890
+#     if d%3==0:
+#       print(d)
+#     a+=1
+
+# a=1
+# while a<=50:
+#     print(a*5)
+#     a+=1
+
+# counter=0
+# while counter<5:
+#     print("nav gurukul")
+#     counter+=1
+# print("1baar print hoga")
+
+# i=0
+# while i<=40:
+#     print("even",i)
+#     i+=2
+# i=21
+# while i<=40:
+#     print("odd",i)
+#     i+=2
+
+# i=int(input("enter any number:"))
+# # i=0
+# while i<=100:
+#     if i%3==0 and i%7==0:
+#         print(i,"nav gurukul")
+#     elif i%3==0:
+#         print(i,"nav")
+#     elif i%7==0:
+#         print(i,"gurukul")
+#     else:
+#         print(i)
+#     i+=1    
+
+
+# s=1
+# while s<=100:
+#     print(s)
+#     s+=1
+
+
+# i=0
+# while i<=10:
+#     print(i,end="  ")
+#     i+=1
+
+# i=0
+# while i<=10:
+#     i=i+1
+#     print(i)
+#     # i=i+1
+    
+# i=0
+# while i<=10:
+#     i=i+1
+# print(i)
+
+# d=1
+# while d<=100:
+#     if d%7==0:
+#         print(d,end=" ")
+#     d+=1
+
+# i=0 taken  5049 output..
+# i=0
+# sum=0
+# while i<100:
+#     # print(i)
+#     i+=1
+#     sum=sum+i
+# print(sum)
+    
+#  i=1 taken output 5050
+
+# i=1
+# sum=0
+# while i<100:
+#     # print(i)
+#     i+=1
+#     sum=sum+i
+# print(sum)
+
+# in condition i<=100 then output 5151
+# i=0
+# sum=0
+# while i<=100:
+#     # print(i)
+#     i+=1
+#     sum=sum+i
+# print(sum)
+
+# sum of 100
+# i=1
+# sum=0
+# while i<=10:
+#    a=int(input("enter any number:"))
+#    sum=sum+a
+#    i+=1
+#    print(sum)   
+# print(sum)
+
+      # number is divisable 7 then print ....
+# a=1
+# while a<100:
+#     if a%7==0:
+#         print(a,end=" ")
+#     a+=1
+
+# all odd numbers 50 to 100...
+# i=50
+# while i<=100:
+#     if i%2!=0:
+#         print(i)
+#     i+=1
+
+# i=400
+# while i>=350:
+#     c=i-300
+#     if c%2!=0:
+#         print(c)
+#     i-=1
+
+# 1 to 10 numbers....
+# a=-1
+# while a>=(-10):
+#     print(-a)
+#     a-=1
+
+    #  
+# a=1
+# while (a):
+#     print(-(a))
+    
+# a=1
+# while a!=11:
+#     print(a)
+#     a-=-1
+    
+# i=1
+# while i<=5:
+#     d="*"
+#     print(i*d)
+#     i+=1
+
+# i=5
+# while i>0:
+#     c="#"
+#     print(i*c)
+#     i-=1
+# i=5
+# while i>0:
+#     d="@"
+#     print(i*   d)
+#     i=i-1
+
+# i=1
+# while i<100:
+#     if i%2==0:
+#          print(-i,end="  ")
+#         # pass
+#     else:
+#         print(i,end=" ")
+#     i+=1
+# i=0
+# while i<100:
+#     print("India..love u ....",end="   ")
+#     i+=1
+# i=1
+# while i<100 :
+#     print("#"*i)
+#     i=i+1
+# sum=0
+# i=0
+# while i<10:
+#     a=int(input("enter number:"))
+#     sum=i+a
+#     i+=1
+# print (sum)
+    
+# a=156
+# while a<167:
+#     d=a-156
+#     print(d)
+#     a+=1
+
+# a=int(input("enter any number:"))
+# i=0
+# while i<4:
+#     if a<0:
+#         print(a,"negative")
+#     elif a>0:
+#         print(a,"positive")
+#     i+=1
+
+# a=0
+# while a<100:
+#     if a==2 or a==3 or a==5 or a==7:
+#         print(a,"prime")
+#     elif a%2==0 or a%3==0 or a%5==0 or a%7==0:
+#         print(a,"not a prime")
+#     else:
+#         print(a,"prime")
+#     a+=1
+
+# pattern like 55555
+            #  44444
+            #  33333
+            #  22222
+            #  11111  
+# i=5
+# while i>=1:
+#     print(str(i)*5)
+#     i-=1
+
+
+# i=1
+# while i<=5:
+#     print(str(i)*5)
+#     i+=1
+# i=1
+# while i<=5:
+#     print(str(i)*i)
+#     i+=1
+# i=5
+# while i>=1:
+#     print(str(i)*i)
+#     i-=1
+
+# break.....
+
+# index=0
+# while 1:
+#     print(index," ",end=" ")
+#     index=index+1
+#     if index==10:
+#         break
+#     print("found at ",index,"location")
+
+# continue...
+
+# i=0
+# while i< 10:
+#     i+=1
+#     # print(i)
+#     if i==5:
+#         print("skipped num:",i)
+#         continue
+#     print(i)
+
+# i=1
+# while i<100:
+#     if i%7==0:
+#         print(i,end=" ")
+#     i+=1
+# counter = 0
+# string = "navgurukul"
+# while (counter < len(string)):
+#     counter += 1
+#     if string[counter] =="n":
+#         continue
+#     if string[counter] =="u":
+#         continue
+#     print(string[counter])
+#     print("The end", string[counter])
+
+# i=1
+# while i<=20:
+#     if i%2==0:
+#         print(i,"even")
+    # i=i+1
+
+# a=10
+# while a<=300:
+#     print(a,end=",")
+#     a+=10
+
+# 
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# i=1
+# while i<=10:
+#     print(i,i*i)
+#     i+=1
+
+# i=105
+# while i>=1:
+#     if i%7==0:
+#         print(i)
+    # i-=1
+
+# i=105
+# while i>=7:
+#     print(i,end=",")
+#     i-=7
+
+# i=10
+# while i>=1:
+#     print(i,end=" ")   
+#     i-=1
+
+# sum 10 natural number...
+# i=1
+# sum=0
+# while i<=10:
+#     sum=sum+i
+#     i+=1
+# print(sum)
+
+# sum of even numbers in d/e 10
+# i=0
+# sum=0
+# while i<=10:
+#     sum=sum+i
+#     i+=2
+#     print(sum)
+# i=1
+# sum=0
+# while i<=20:
+#     if i%2==0:
+#         print(i)
+#         sum=sum+i
+#     i=i+1
+# print(sum)
+# sum=0
+# while i<=10:
+#     sum=sum+i
+#     i+=2
+#     print(sum))
+
+# a=int(input("enter number:"))
+# d=int(input("enter number:"))
+# while a<=d :
+#     if a%2==0:
+#         print(a)
+#     a+=1
+# while d<=a:
+#     if d%2==0:
+#         print(d)
+#     d=d+1    
+
+# reverse number.......
+# n=int(input("enter any number:"))
+# while n!=0:
+#     d=n%10
+#     print(d,end="")
+#     n=n//10
+
+# # sum of digits...
+# a=int(input("enter any number:"))
+# sum=0
+# while a!=0:
+#     sum=sum+(a%10)
+#     a=a//10
+# print(sum)
+
+# product of digits....
+# a=int(input("enter number:"))
+# pr=1
+# while a!=0:
+#     d=a%10
+#     pr=pr*d
+#     a=a//10
+#     print(pr)
+
+
+# k=int(input('enter any number:'))
+# while k!=0:
+#     d=k%10
+#     k=k//10
+#     if d==1:
+#         print("one")
+#     elif d==2:
+#         print("two")
+#     elif d==3:
+#         print("three")
+#     elif d==4:
+#         print("four")
+#     elif d==5:
+#         print("five")
+#     elif d==6:
+#         print("six")
+#     elif d==7:
+#         print("seven")
+#     elif d==8:
+#         print("eight")
+#     elif d==9:
+#         print("nine")
+#     elif d==0:
+#         print("zero")
+# print(d,end=" ")
+
+
+# pattern....
+
+# n=input("enter any number:")
+# for i in n:
+#     print(i)
+# a=int(input("enter number:"))
+
+
+
+
+# a=int(input("enter any number"))
+# while a>0:
+#     print(a)
+
+
+
+
+
+# for x in range(2,30,3):
+#     print(x)
+  
+# # get the input as string        
+# n = "41325"
+# pattern(n)
+# # x= int(input("enter the number"))
+# y= int(input("enter the number"))
+# z= int(input("enter the number"))
+# if x>y>z or z>y>x:
+#     print("y is second maxmum number")
+# elif y>z>x or y>z>x:
+#     print("z is second maxmum number")
+# else:
+# #     print("x is second maxmum number")
+
+# 17 question
+# a=int(input("enter number"))
+# b=a
+# sum=0
+# while a>0:
+#     d=a%10
+#     a=a//10
+#     print(a)
+#     c=d**3
+#     sum=sum+c
+#     print(c)
+# print(sum)
+# if sum==b:
+#     print("armstrong")
+# else:
+#     print("not a armstrong")
+
+#  22 question:
+# i=0
+# sum=0
+# while i<10:
+#     a=int(input("enter"))
+#     sum=sum+a
+#     i=i+1
+# print(sum)
+# avg=sum/10
+# print(avg)
+
+# polindrom or not............
+# i=0
+# a=int(input("enter any number:"))
+# while i<=a:
+#     b=a%10
+#     # a=a//10
+#     i+=1
+#     print(b,end="")
+#     a=a//10
+
+# you tube...........
+# def prime(x, y):
+#     prime_list = []
+#     for i in range(x, y):
+#         if i == 0 or i == 1:
+#             continue
+#         else:
+#             for j in range(2, int(i/2)+1):
+#                 if i % j == 0:
+#                     break
+#             else:
+#                 prime_list.append(i)
+#     return prime_list
+
+# start to end digit print............
+# n=input("enter any number:")
+# for i in n:
+#      print(i)
+
+
+#  factorial......
+# i=1
+# pr=1
+# a=int(input("enter the number."))
+# while i<=a:
+#   pr=pr*i
+#   i+=1
+# print(pr)
+
+
+# strong number............
+# sum=0
+# num=int(input("entr number:"))
+# b=num
+# while num:
+#     i=1
+#     fact=1
+#     d=num%10 
+#     print(d)
+#     while i<=d:
+#         fact=fact*i
+#         print(fact)
+#         i=i+1
+#     sum=sum+fact
+#     print(sum)
+#     num=num//10
+# if sum==b:
+#     print(b,"strong number")
+# else:
+#     print(b,"not a strong number")
+
+
+
+
+# armstrong number.................
+# a=int(input("enter number"))
+# s=len(str(a))
+# print(s)
+# b=a
+# sum=0
+# while a>0:
+#     d=a%10
+#     a=a//10
+#     print(a)
+#     c=d**s
+#     sum=sum+c
+#     print(c)
+# print(sum)
+# if sum==b:
+#     print("armstrong")
+# else:
+#     print("not a armstrong")
+
+
+# harshad number................
+# i=0
+# sum=0
+# a=int(input("enter number"))
+# b=a
+# while i<=10:
+#     d=a%10
+#     a=a//10
+#     # print(b)
+#     sum=sum+d
+#     i=i+1
+#     # print(sum)
+# print(sum)
+# if b%sum==0:
+#     print("harshad number")
+# else:
+#     print("not a harshad number")
+    
+#happy number..............
+# i=0
+# sum=0
+# a=int(input("enter number"))
+# while i<=10:
+#     d=a%10
+#     c=d**2
+#     a=a//10
+#     sum=sum+c
+#     i=i+1
+# print(sum)
+# if sum==1:
+#     print("happy number")
+# else:
+#     print("sad number")
+
+
+# # factors.............
+# i=1
+# a=int(input("enter number"))
+# while i<=a:
+#     if a%i==0:
+#         print(i,end=" ")  
+#     i=i+1
+
+# fobinacci series....................
+# n=int(input("enter any number:"))
+# x=1
+# y=1
+# z=0
+# while z<n:
+#     print(z,end=" ")
+#     x=y
+#     y=z
+#     z=x+y
+
+
+# i=1
+# n=int(input("enter any number:"))
+# while i<=n:
+#     print("1 /",i,"! ",end="+ ")
+#     i+=1
+
+# i=1
+# a=2
+# n=int(input("enter any number:"))
+# while i<=n:
+#     print(str(a)*i,end=",")
+#     i+=1
+
+# i=1
+# n=int(input("enter any number:"))
+# while i<=n:
+#     print(i*i,end="+")
+#     i+=1
+
+# i=1
+# sum=0
+# n=int(input("enter any number:"))
+# while i<=n:
+#     cub=i**3
+#     sum=sum+cub
+#     print(cub,end="+")
+#     i+=1
+
+# i=1
+# n=int(input("enter any number:"))
+# pr=1
+# while i<=n:
+#     pr=pr*i
+#     print(pr,end="+")
+#     i+=1
+
+# # # sum of even and add.............
+# a=int(input("enter any number:"))
+# b=int(input("enter any number:"))
+# asum=0
+# bsum=0
+# while a<=b:
+#     if a%2==0:
+#        asum=asum+a
+#     elif a%2!=0:
+#         bsum=bsum+a
+#     a=a+1
+# print(bsum)
+# print(asum)
+
+
+# palindrome number..............                                                                                                                                                      r not .............numbers
+# rev=0
+# a=int(input("enter any number:"))
+# temp=a
+# while a>0:
+#     m=a%10
+#     rev=rev*10+m
+#     a=a//10
+#     # a+=1
+#     # print(rev)
+# if temp==rev:
+#     print("polindrom",rev,temp)
+# else:
+#     print("not a polindrom",rev,temp)
+# 
+# # character palindrome............
+# type=input("enter anything:")
+# b=type[::-1]
+# if type==b:
+#     print("palindrome")
+# else:
+#     print("Not a palindrome")
+
+
+# i=0
+# s=[21,34,56,76,44,99]
+# while s[i]>len(s):
+#     if s[i]%2!=0:
+#         print(s[i])
+#     i+=1
+
+
+
+
+
+
+# # 38............
+# i=1500
+# while i<=2700:
+#     # if i%7==0 and i%5==0:
+#     #     print(i,end=" ")
+#     if i%7==0:
+#         pass 
+#         """print(i,"divisable by 7 only")"""
+#         if i%5==0:
+#            print(i,"divisable by 5 only")
+#     # else:
+#     #     print(i,"not divisable")
+#     i+=1
+# 40........
+# a=int(input("enter number:"))
+# b=int(input("enter number:"))
+# sum=a+b
+# i=15
+# while i<=20:
+#     if sum==i:
+#         print("between")
+#         break
+#     i+=1
+
+    
+    
+# i=5
+# while i>=1:
+#     print(str(i)*i)
+#     i-=1
+# i=5
+# while i>=1:
+#     print(str(i)*i)
+#     i-=1
+# i=1
+# while i<=5:
+#     print((str(i)+str(i))*i)
+#     i+=1
+
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(j,end=" ")
+#         j=j+1
+#     i=i+1
+#     print( )
+# i=5
+# while i>=1:
+#     j=5
+#     while j>=1:
+#         print(j,end=" ")
+#         j=j-1
+#     i=i-1
+#     print( )
+# i=1
+# while i<=5:
+#     print(str(i*i)*i)
+#     i+=1
+
+# j=1
+# while j<=5:
+#     i=1
+#     while i<=j:
+#       print(j*2-1,end=" ")
+#       i+=1
+#     j+=1
+#     print(" ")
+
+
+
+# y=int(input("enter year:"))
+# mo=int(input("enter month"))
+# d=int(input("enter date:"))
+# h=int(input("enter hours:"))
+# m=int(input("enter minute:"))
+# s=int(input("enter second"))
+# if y!=0 and 0!=mo<=12 and 0!=d<=31:
+#     print(y,"-",mo,"-",d)
+# else:
+#     print("you enter wrong ")
+# if h<24 and m<60 and s<60:
+#     print(h,":",m,":",s)
+# else:
+#     print("entered wrong time")
+
+# months seasons................
+# m=input("enter month:")
+# if m== "feb"or m=="jan":
+#     print("winter")
+# elif m=="march" or m=="apl":
+#     print("spring")
+# elif m=="may" or m=="june":
+#     print("summer")
+# elif m=="jul"or m=="aug":
+#     print("monsoon")
+# elif m=="sep" or m=="oct":
+#     print("autumn")
+# elif m=="nov" or m=="dec":
+#     print("pre winter")
+
+# m=input("enter season")
+# if m=="winter":
+#     print("jan and feb")
+# elif m=="spring":
+#     print("march apl")
+# elif m=="summer":
+#     print("may and june")
+# elif m=="monsoon":
+#     print("jul and aug")
+# elif m=="autumn":
+#     print("sep and oct")
+# elif m=="prewinter":
+#     print("nov and dec")
+
+# str1="amita"str1
+# str2="my blog"
+# str="#blog"
+# str="my 1st blog"
+# print(str1.isalpha())
+# print(str2.isalpha())
+
+
+
+
+
+
+
+
+
+
+
